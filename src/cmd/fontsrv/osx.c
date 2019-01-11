e8dbfc48da7fb91b9fe9b0aa309544b21b5d8b3a
marius a. eriksen <marius@grailbio.com>
2019-01-10 21:26:38 -0800

    fontsrv: support for Drive

--- src/cmd/fontsrv/osx.c
+++ src/cmd/fontsrv/osx.c
@@ -64,6 +64,7 @@ static struct {
 	{"Plex", Zero | Endash },
 	{"Ideal", Zero | Tab | Lnum | Endash },
 	{"Whitney", Zero | Tab | Lnum |  SS11 | SS17 | Endash },
+	{"Drive", Zero | Tab | Lnum |  Endash },
 	{"Fira", Zero | Tab | Lnum },
 	{"SF", Zero | Tab | Lnum | Endash },
 	{"Syntax", Zero | Tab | Lnum | Endash },
